4ce457c560f20c5f129dde67119038bc8e4ebf6e
koseungmin <30397679+koseungmin@users.noreply.github.com>
2022-03-11 09:59:08 +0900

    Update accu_ml_parse_call.py

--- tests/dags/accu_ml_parse_call.py
+++ tests/dags/accu_ml_parse_call.py
@@ -22,7 +22,7 @@ default_args = {
 }
 
 dag = DAG(
-    'accutuning_ml_parse_call', default_args=default_args, schedule_interval=timedelta(minutes=10))
+    'accutuning_ml_parse_call3', default_args=default_args, schedule_interval=timedelta(minutes=10))
 
 start = DummyOperator(task_id='start', dag=dag)
 
@@ -32,8 +32,26 @@ volume_mount = k8s.V1VolumeMount(
 )
 volume = k8s.V1Volume(
     name='test-volume',
-    persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name='test-volume', read_only=False),
+#     persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name='test-volume', read_only=False),
+    host_path=k8s.V1HostPathVolumeSource(path='/workspace'),
 )
+
+
+# volume = k8s.V1Volume(
+#     name='workspace-3-volume',
+#     # persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name='workspace-volume-3-claim'),
+#     host_path=k8s.V1HostPathVolumeSource(path='/workspace'),
+# )
+
+# volume_mounts = [
+#     k8s.V1VolumeMount(
+#         mount_path='/workspace', name='workspace-3-volume', sub_path=None,
+#         read_only=False
+#     )
+# ]
+
+
+
 configmaps = [
     k8s.V1EnvFromSource(config_map_ref=k8s.V1ConfigMapEnvSource(name='airflow-test-1')),
 ]  
@@ -55,7 +73,8 @@ init_container_volume_mounts = [
     
 ml_parse_pre = KubernetesPodOperator(
     namespace='default',
-    image="pooh97/accu-app:latest",    
+#     image='{{dag_run.conf["ACCUTUNING_APP_IMAGE"]}}',    
+    image='pooh97/accu-app:latest',    
     volumes=[volume],
     volume_mounts=[volume_mount],
     name="ml_parse_pre",
@@ -63,36 +82,17 @@ ml_parse_pre = KubernetesPodOperator(
 #     init_containers=[init_container],
     #env_vars={'ACCUTUNING_LOG_LEVEL': '{{dag_run.conf["ACCUTUNING_LOG_LEVEL"] if dag_run else "" }}', 'ACCUTUNING_WORKSPACE':'{{dag_run.conf["ACCUTUNING_WORKSPACE"] if dag_run else "" }}'},
     env_vars={
-#         'ACCUTUNING_CALL_API_KIND': 'PARSE_PRE', 
-#               'ACCUTUNING_EXPERIMENT_ID': '19',
-#               'ACCUTUNING_UUID': '4043104546ca4c0597ba5341607ba06f',
-#               'ACCUTUNING_TIMEOUT': '200',
-              'ACCUTUNING_HOST_API_URL' : '10.100.92.116:8080',
-#               'ACCUTUNING_HOST_API_URL' : 'host.minikube.internal:8080',
-              'ACCUTUNING_WORKSPACE':'/workspace/',
-              'ACCUTUNING_WORKER_WORKSPACE':'/workspace/experiment_0019/experimentprocess_0050',
-              'ACCUTUNING_LOG_LEVEL':'INFO',
-              'ACCUTUNING_DEBUG':'1',
-              'ACCUTUNING_INIT':'0',
-              'ACCUTUNING_AUTH':'0',
-              'ACCUTUNING_NOTEBOOK':'0',
-              'ACCUTUNING_DB_ENGINE':'sqlite',
-              'ACCUTUNING_K8S_USE':'0',
-              'ACCUTUNING_K8S_VOLUME_MOUNT_PATH':'${PWD}/.workspace',
-              'ACCUTUNING_CELERY_ONLY':'0',
-              'ACCUTUNING_CELERY':'0',
-              'ACCUTUNING_CELERY_BROKER':'amqp://rabbitmq:5672/',
-              'ACCUTUNING_USE_LABELER':'1',
-              'ACCUTUNING_USE_CLUSTERING':'1',
-              'DJANGO_SETTINGS_MODULE':'accutuning.settings',
-              'AWS_S3_DEFAULT_BUCKET':'accutuningawsbucket',
-              'AWS_DEFAULT_REGION':'ap-northeast-2',
-              'ACCUTUNING_USE_SSO':'0'
+              'ACCUTUNING_WORKSPACE':'{{dag_run.conf["ACCUTUNING_WORKSPACE"]}}',
+              'ACCUTUNING_LOG_LEVEL':'{{dag_run.conf["ACCUTUNING_LOG_LEVEL"]}}',
+              'ACCUTUNING_USE_LABELER':'{{dag_run.conf["ACCUTUNING_USE_LABELER"]}}',
+              'ACCUTUNING_USE_CLUSTERING':'{{dag_run.conf["ACCUTUNING_USE_CLUSTERING"]}}',
+              'DJANGO_SETTINGS_MODULE':'{{dag_run.conf["DJANGO_SETTINGS_MODULE"]}}'     
              },
 #     cmds=["bash", "-cx"],
 #     arguments=["python3 /code/manage.py ml_parse_pre --experiment=19 --uuid='4043104546ca4c0597ba5341607ba06f' --timeout=200"],
-    cmds=["bash", "-cx", "python3"],
-    arguments=["/code/manage.py", "ml_parse_pre", "--experiment=19",  "--uuid='4043104546ca4c0597ba5341607ba06f'", "--timeout=200"],   
+#     cmds=["bash", "-cx", "python3"],
+    cmds=["python3"],
+    arguments=["/code/manage.py", "ml_parse_pre", "--experiment={{dag_run.conf['ACCUTUNING_EXPERIMENT_ID']}}",  "--uuid={{dag_run.conf['ACCUTUNING_UUID']}}", "--timeout={{dag_run.conf['ACCUTUNING_TIMEOUT']}}"],   
     
     get_logs=True,
     dag=dag,    
@@ -100,13 +100,13 @@ ml_parse_pre = KubernetesPodOperator(
 
 ml_parse_main = KubernetesPodOperator(
     namespace='default',
-    image="pooh97/accu-worker:latest",    
+    image="{{dag_run.conf['ACCUTUNING_WORKER_IMAGE']}}",    
     volumes=[volume],
     volume_mounts=[volume_mount],
     name="ml_parse_main",
     task_id="ml_parse_main",
 #     env_vars={'ACCUTUNING_LOG_LEVEL': '{{dag_run.conf["ACCUTUNING_LOG_LEVEL"] if dag_run else "" }}', 'ACCUTUNING_WORKSPACE':'{{dag_run.conf["ACCUTUNING_WORKSPACE"] if dag_run else "" }}'},
-    env_vars={'ACCUTUNING_LOG_LEVEL': 'INFO', 'ACCUTUNING_WORKSPACE':'/workspace/experiment_0019/experimentprocess_0050'},
+    env_vars={'ACCUTUNING_LOG_LEVEL': '{{dag_run.conf["ACCUTUNING_LOG_LEVEL"]}}', 'ACCUTUNING_WORKSPACE':'{{dag_run.conf["ACCUTUNING_WORKER_WORKSPACE"]}}'},
     get_logs=True,
     dag=dag,    
 )
@@ -116,56 +116,60 @@ ml_parse_main = KubernetesPodOperator(
 # python3 /code/manage.py ml_parse_pre --experiment=19 --uuid=$ACCUTUNING_UUID --timeout=$ACCUTUNING_TIMEOUT
 # env
 # python3 /code/manage.py ml_parse_post --experiment=$ACCUTUNING_EXPERIMENT_ID --uuid=$ACCUTUNING_UUID --timeout=$ACCUTUNING_TIMEOUT
-ml_parse_post = KubernetesPodOperator(
+ml_parse_success = KubernetesPodOperator(
     namespace='default',
-    image="pooh97/accu-app:latest",    
+    image='{{dag_run.conf["ACCUTUNING_APP_IMAGE"]}}',        
     volumes=[volume],
     volume_mounts=[volume_mount],
-    name="ml_parse_post",
-    task_id="ml_parse_post",
+    name="ml_parse_success",
+    task_id="ml_parse_success",
 #     init_containers=[init_container],
     #env_vars={'ACCUTUNING_LOG_LEVEL': '{{dag_run.conf["ACCUTUNING_LOG_LEVEL"] if dag_run else "" }}', 'ACCUTUNING_WORKSPACE':'{{dag_run.conf["ACCUTUNING_WORKSPACE"] if dag_run else "" }}'},
     env_vars={
-#         'ACCUTUNING_CALL_API_KIND': 'PARSE_POST', 
-#               'ACCUTUNING_EXPERIMENT_ID': '19',
-#               'ACCUTUNING_UUID': '4043104546ca4c0597ba5341607ba06f',
-#               'ACCUTUNING_TIMEOUT': '200',
-              'ACCUTUNING_HOST_API_URL' : '10.100.92.116:8080',
-#               'ACCUTUNING_HOST_API_URL' : 'host.minikube.internal:8080',
-              'ACCUTUNING_WORKSPACE':'/workspace/',
-              'ACCUTUNING_WORKER_WORKSPACE':'/workspace/experiment_0019/experimentprocess_0050',
-              'ACCUTUNING_LOG_LEVEL':'INFO',
-              'ACCUTUNING_DEBUG':'1',
-              'ACCUTUNING_INIT':'0',
-              'ACCUTUNING_AUTH':'0',
-              'ACCUTUNING_NOTEBOOK':'0',
-              'ACCUTUNING_DB_ENGINE':'sqlite',
-#               'ACCUTUNING_K8S_USE':'0',
-#               'ACCUTUNING_K8S_VOLUME_MOUNT_PATH':'${PWD}/.workspace',
-#               'ACCUTUNING_CELERY_ONLY':'0',
-#               'ACCUTUNING_CELERY':'0',
-#               'ACCUTUNING_CELERY_BROKER':'amqp://rabbitmq:5672/',
-              'ACCUTUNING_USE_LABELER':'1',
-              'ACCUTUNING_USE_CLUSTERING':'1',
-              'DJANGO_SETTINGS_MODULE':'accutuning.settings',
-#               'AWS_S3_DEFAULT_BUCKET':'accutuningawsbucket',
-#               'AWS_DEFAULT_REGION':'ap-northeast-2',
-#               'ACCUTUNING_USE_SSO':'0'
+              'ACCUTUNING_WORKSPACE':'{{dag_run.conf["ACCUTUNING_WORKSPACE"]}}',
+              'ACCUTUNING_LOG_LEVEL':'{{dag_run.conf["ACCUTUNING_LOG_LEVEL"]}}',
+              'ACCUTUNING_USE_LABELER':'{{dag_run.conf["ACCUTUNING_USE_LABELER"]}}',
+              'ACCUTUNING_USE_CLUSTERING':'{{dag_run.conf["ACCUTUNING_USE_CLUSTERING"]}}',
+              'DJANGO_SETTINGS_MODULE':'{{dag_run.conf["DJANGO_SETTINGS_MODULE"]}}'     
              },
 #     cmds=["bash", "-cx"],
-    cmds=["bash", "-cx", "python3"],
-    arguments=["/code/manage.py", "ml_parse_post", "--experiment=19",  "--uuid='4043104546ca4c0597ba5341607ba06f'", "--timeout=200"],   
+#     cmds=["bash", "-cx", "python3"],
+    cmds=["python3"],
+    arguments=["/code/manage.py", "ml_parse_post", "--experiment={{dag_run.conf['ACCUTUNING_EXPERIMENT_ID']}}",  "--uuid={{dag_run.conf['ACCUTUNING_UUID']}}", "--timeout={{dag_run.conf['ACCUTUNING_TIMEOUT']}}"],   
     get_logs=True,
     dag=dag,        
     trigger_rule='all_success',
 )
 
-
-failure = DummyOperator(
-    task_id='failure',
-    trigger_rule='one_failed',    
-    dag=dag,
+ml_parse_fail = KubernetesPodOperator(
+    namespace='default',
+    image="pooh97/accu-app:latest",    
+    volumes=[volume],
+    volume_mounts=[volume_mount],
+    name="ml_parse_fail",
+    task_id="ml_parse_fail",
+#     init_containers=[init_container],
+    #env_vars={'ACCUTUNING_LOG_LEVEL': '{{dag_run.conf["ACCUTUNING_LOG_LEVEL"] if dag_run else "" }}', 'ACCUTUNING_WORKSPACE':'{{dag_run.conf["ACCUTUNING_WORKSPACE"] if dag_run else "" }}'},
+    env_vars={
+              'ACCUTUNING_WORKSPACE':'{{dag_run.conf["ACCUTUNING_WORKSPACE"]}}',
+              'ACCUTUNING_LOG_LEVEL':'{{dag_run.conf["ACCUTUNING_LOG_LEVEL"]}}',
+              'ACCUTUNING_USE_LABELER':'{{dag_run.conf["ACCUTUNING_USE_LABELER"]}}',
+              'ACCUTUNING_USE_CLUSTERING':'{{dag_run.conf["ACCUTUNING_USE_CLUSTERING"]}}',
+              'DJANGO_SETTINGS_MODULE':'{{dag_run.conf["DJANGO_SETTINGS_MODULE"]}}'        
+             },
+#     cmds=["bash", "-cx"],
+    cmds=["python3"],
+    arguments=["/code/manage.py", "ml_parse_fail", "--experiment={{dag_run.conf['ACCUTUNING_EXPERIMENT_ID']}}",  "--uuid={{dag_run.conf['ACCUTUNING_UUID']}}", "--timeout={{dag_run.conf['ACCUTUNING_TIMEOUT']}}"],   
+    get_logs=True,
+    dag=dag,        
+    trigger_rule='one_failed',
 )
+
+# failure = DummyOperator(
+#     task_id='failure',
+#     trigger_rule='one_failed',    
+#     dag=dag,
+# )
 
 ## one_success로 해야 skip된 task를 무시함
 end = DummyOperator(
@@ -200,8 +204,8 @@ options = ['ml_parse_post', 'failure']
 
 start >> Label("app 중 ml_parse_pre Call") >> ml_parse_pre >> Label("common_module worker 중 Call") >> ml_parse_main 
 
-ml_parse_main >> Label("worker 작업 성공시(app 중 ml_parse_post Call)") >> ml_parse_post >> end
-ml_parse_main >> Label("worker 작업 실패시") >> failure >> end
+ml_parse_main >> Label("worker 작업 성공시(app 중 ml_parse_success Call)") >> ml_parse_success >> end
+ml_parse_main >> Label("worker 작업 실패시(app 중 ml_parse_fail Call)") >> ml_parse_fail >> end
 
 # start >> ml_parse_pre >> ml_parse_main >> check_situation
 # check_situation >> ml_parse_post >> success >> finish 
